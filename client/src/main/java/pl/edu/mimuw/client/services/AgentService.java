fa34c0f1613ff6af820e835719dfe857873fb4eb
m-naruniec <micnar12@gmail.com>
2019-11-18 23:42:19 +0100

    client: Add AgentService with direct calls

---
+++ client/src/main/java/pl/edu/mimuw/client/services/AgentService.java
@@ -0,0 +1,73 @@
+package pl.edu.mimuw.client.services;
+
+import org.springframework.stereotype.Service;
+import pl.edu.mimuw.cloudatlas.agent.IllegalAttributeException;
+import pl.edu.mimuw.cloudatlas.agent.NoSuchZoneException;
+import pl.edu.mimuw.cloudatlas.agent.QueryParsingException;
+import pl.edu.mimuw.cloudatlas.agent.api.IAgentAPI;
+import pl.edu.mimuw.cloudatlas.model.Value;
+import pl.edu.mimuw.cloudatlas.model.ValueContact;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Service
+public class AgentService implements IAgentAPI {
+	private IAgentAPI agentAPI;
+	private List<String> storedZones = new ArrayList<>();
+
+	public AgentService() throws Exception {
+		Registry registry = LocateRegistry.getRegistry();
+		agentAPI = (IAgentAPI) registry.lookup("AgentAPI");
+		storedZones.addAll(agentAPI.getStoredZones());
+		storedZones.sort(Comparator.naturalOrder());
+	}
+
+	@Override
+	public Set<String> getStoredZones() throws RemoteException {
+		return agentAPI.getStoredZones();
+	}
+
+	public List<String> getStoredZonesList() {
+		return storedZones;
+	}
+
+	@Override
+	public Map<String, Value> getZoneAttributes(String zone, boolean excludeQueries)
+			throws RemoteException, NoSuchZoneException {
+		return agentAPI.getZoneAttributes(zone, excludeQueries);
+	}
+
+	@Override
+	public void upsertZoneAttributes(String zone, Map<String, Value> attributes)
+			throws RemoteException, NoSuchZoneException, IllegalAttributeException {
+		agentAPI.upsertZoneAttributes(zone, attributes);
+	}
+
+	@Override
+	public void installQuery(String name, String query)
+			throws RemoteException, IllegalAttributeException, QueryParsingException {
+		agentAPI.installQuery(name, query);
+	}
+
+	@Override
+	public void uninstallQuery(String name) throws RemoteException {
+		agentAPI.uninstallQuery(name);
+	}
+
+	@Override
+	public void setFallbackContacts(Set<ValueContact> contacts) throws RemoteException {
+		agentAPI.setFallbackContacts(contacts);
+	}
+
+	@Override
+	public Set<ValueContact> getFallbackContacts() throws RemoteException {
+		return agentAPI.getFallbackContacts();
+	}
+}
