ebede7ed60ccf9a92291c635791b7c0530fb903f
m-naruniec <micnar12@gmail.com>
2019-11-18 23:45:59 +0100

    client: Add installQuery business logic

--- client/src/main/java/pl/edu/mimuw/client/controllers/InstallQueryController.java
+++ client/src/main/java/pl/edu/mimuw/client/controllers/InstallQueryController.java
@@ -1,13 +1,18 @@
 package pl.edu.mimuw.client.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import pl.edu.mimuw.client.services.AgentService;
 
 @Controller
 public class InstallQueryController {
+	@Autowired
+	private AgentService agentService;
+
 	public static class InstallForm {
 		private String name;
 		private String query;
@@ -27,6 +32,10 @@ public class InstallQueryController {
 		public void setQuery(String query) {
 			this.query = query;
 		}
+
+		public boolean isFilled() {
+			return name != null && query != null && !name.isEmpty() && !query.isEmpty();
+		}
 	}
 
 	@GetMapping("/install")
@@ -36,8 +45,18 @@ public class InstallQueryController {
 	}
 
 	@PostMapping("/install")
-	public String installQuery(@ModelAttribute InstallForm installForm) {
-		// TODO - implement logic
+	public String installQuery(Model model, @ModelAttribute InstallForm installForm) {
+		String message = "SUCCESS";
+		try {
+			if (!installForm.isFilled()) {
+				throw new IllegalArgumentException("Provide values in all fields.");
+			}
+			agentService.installQuery(installForm.getName(), installForm.getQuery());
+		} catch (Exception e) {
+			e.printStackTrace();
+			message = "ERROR:" + e.getMessage();
+		}
+		model.addAttribute("message", message);
 		return "install";
 	}
 }
