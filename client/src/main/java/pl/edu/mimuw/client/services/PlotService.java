c68373869dafe7154de36aec81ef0d091374e7ef
m-naruniec <micnar12@gmail.com>
2020-01-18 20:33:59 +0100

    client: Simplify and fix PlotService

--- client/src/main/java/pl/edu/mimuw/client/services/PlotService.java
+++ client/src/main/java/pl/edu/mimuw/client/services/PlotService.java
@@ -48,48 +48,45 @@ public class PlotService {
 		this.mapper = mapper;
 	}
 
-	public JsonNode getPlotData(String zone, String attribute) {
+	public synchronized JsonNode getPlotData(String zone, String attribute) {
 		ObjectNode root = mapper.createObjectNode();
 		ArrayNode data = mapper.createArrayNode();
 		ArrayNode data_type = mapper.createArrayNode();
 		ArrayNode label = mapper.createArrayNode();
 
 		Map<String, List<TimedValue>> attrMap;
-		synchronized (plotMap) {
-			attrMap = plotMap.get(zone);
-			if (attrMap == null) {
-				root.put("error", "Zone not found.");
-				return root;
-			}
+		attrMap = plotMap.get(zone);
+		if (attrMap == null) {
+			root.put("error", "Zone not found.");
+			return root;
 		}
-		synchronized (attrMap) {
-			List<TimedValue> timeList = attrMap.get(attribute);
-			if (timeList == null) {
-				root.put("error", "Attribute not found for this zone.");
-				return root;
-			}
 
-			for (TimedValue timedValue : timeList) {
-				label.add(timedValue.time);
-				if (timedValue.value == null) {
-					data.add((JsonNode) null);
-					data_type.add((JsonNode) null);
-				} else {
-					switch (timedValue.value.getType().getPrimaryType()) {
-						case DOUBLE:
-							data.add(((ValueDouble) timedValue.value).getValue());
-							break;
-						case DURATION:
-							data.add(((ValueDuration) timedValue.value).getValue());
-							break;
-						case INT:
-							data.add(((ValueInt) timedValue.value).getValue());
-							break;
-						default:
-							data.add((JsonNode) null);
-					}
-					data_type.add(timedValue.value.getType().toString());
+		List<TimedValue> timeList = attrMap.get(attribute);
+		if (timeList == null) {
+			root.put("error", "Attribute not found for this zone.");
+			return root;
+		}
+
+		for (TimedValue timedValue : timeList) {
+			label.add(timedValue.time);
+			if (timedValue.value == null) {
+				data.add((JsonNode) null);
+				data_type.add((JsonNode) null);
+			} else {
+				switch (timedValue.value.getType().getPrimaryType()) {
+					case DOUBLE:
+						data.add(((ValueDouble) timedValue.value).getValue());
+						break;
+					case DURATION:
+						data.add(((ValueDuration) timedValue.value).getValue());
+						break;
+					case INT:
+						data.add(((ValueInt) timedValue.value).getValue());
+						break;
+					default:
+						data.add((JsonNode) null);
 				}
+				data_type.add(timedValue.value.getType().toString());
 			}
 		}
 
@@ -100,7 +97,7 @@ public class PlotService {
 	}
 
 	@Scheduled(fixedDelay = COLLECTION_INTERVAL_MS, initialDelay = 1000)
-	public void update() {
+	public synchronized void update() {
 		Set<String> zones;
 		try {
 			zones = agentService.getStoredZones();
@@ -109,27 +106,18 @@ public class PlotService {
 			return;
 		}
 
-		// local snapshot of this.plotMap
-		Map<String, Map<String, List<TimedValue>>> plotMap;
-
-		synchronized (this.plotMap) {
-			for (String zone: new LinkedList<>(this.plotMap.keySet())) {
-				if (!zones.contains(zone)) {
-					this.plotMap.remove(zone);
-				}
+		for (String zone : new LinkedList<>(this.plotMap.keySet())) {
+			if (!zones.contains(zone)) {
+				this.plotMap.remove(zone);
 			}
-			for (String zone: zones) {
-				if (!this.plotMap.containsKey(zone)) {
-					this.plotMap.put(zone, new HashMap<>());
-				}
-			}
-
-			// take snapshot of top level
-			plotMap = (Map) ((HashMap) this.plotMap).clone();
 		}
-		// don't use this.plotMap past this point
+		for (String zone : zones) {
+			if (!this.plotMap.containsKey(zone)) {
+				this.plotMap.put(zone, new HashMap<>());
+			}
+		}
 
-		for (String zone: zones) {
+		for (String zone : zones) {
 			Map<String, List<TimedValue>> attrMap = plotMap.get(zone);
 
 			String now = TIME_FORMAT.format(new Date());
@@ -137,40 +125,37 @@ public class PlotService {
 			try {
 				receivedMap = agentService.getZoneAttributes(zone);
 			} catch (Exception e) {
-				System.out.println("EXCEPTION: " + e.getMessage());
+				System.out.println("Exception when reading zone '" + zone + "'. Skipping.");
 				e.printStackTrace();
 				// we just skip the read if failed
 				continue;
 			}
-			synchronized (attrMap) {
-				for (Map.Entry<String, Value> newEntry: receivedMap.entrySet()) {
-					List<TimedValue> timeList = attrMap.computeIfAbsent(
-							newEntry.getKey(), k -> new LinkedList<>()
-					);
 
-					Value value = newEntry.getValue();
-					value = value == null || value.isNull() || !value.isNumeric() ?
-							null : value;
+			for (Map.Entry<String, Value> newEntry : receivedMap.entrySet()) {
+				List<TimedValue> timeList = attrMap.computeIfAbsent(
+						newEntry.getKey(), k -> new LinkedList<>()
+				);
 
-					timeList.add(new TimedValue(now, value));
-					if (timeList.size() > SAMPLE_CAPACITY) {
-						timeList.remove(0);
-					}
+				Value value = newEntry.getValue();
+				value = value == null || value.isNull() || !value.isNumeric() ?
+						null : value;
+
+				timeList.add(new TimedValue(now, value));
+				if (timeList.size() > SAMPLE_CAPACITY) {
+					timeList.remove(0);
 				}
+			}
 
-				// remove old elements
-				for (String attr: attrMap.keySet()) {
-					if (!receivedMap.containsKey(attr)) {
-						attrMap.remove(attr);
-					}
+			// remove old elements
+			for (String attr : new LinkedList<>(attrMap.keySet())) {
+				if (!receivedMap.containsKey(attr)) {
+					attrMap.remove(attr);
 				}
 			}
 		}
 	}
 
-	public void reset() {
-		synchronized (plotMap) {
-			plotMap.clear();
-		}
+	public synchronized void reset() {
+		plotMap.clear();
 	}
 }
